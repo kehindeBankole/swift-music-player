a2176cd61404885d2d09cde7634be30bd3d40968
kehindeBankole <bankolek1@gmail.com>
2024-01-26 10:37:05 +0100

    Initial Commit

---
+++ music-player/music_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_playerApp.swift
+//  music-player
+//
+//  Created by kehinde on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct music_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
