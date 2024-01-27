cc0e221ff71f2f14023ca577f9cf07455c2e175b
kehindeBankole <bankolek1@gmail.com>
2024-01-27 22:38:39 +0100

    wip

---
+++ music-player/utils/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  music-player
+//
+//  Created by kehinde on 26/01/2024.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+static let background = Color("bg")
+static let musicBlack = Color("customBlack")
+}
